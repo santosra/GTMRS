8f6e45d86f70a7c82b95c20558ad797bc2bcaf45
Evan Norsworthy <enorswo@gmail.com>
2014-04-18 18:43:55 -0400

    formatting

---
+++ InsertStatements.sql
@@ -0,0 +1,6 @@
+INSERT INTO User VALUES ('LozanoG', 'password');
+INSERT INTO Payment_Information 
+	VALUES ('5555555555555555', 'Gloria Lozano', 999, '2017-12-12', 'VISA');
+INSERT INTO Patient
+	VALUES ('LozanoG', 'Gloria', '1990-10-20', 'female', '555 Smallville Drive', 
+	5555555555, 5555555555, 'mom', 1111111111, 120.0, 62.0, 5000.0, '5555555555555555');
