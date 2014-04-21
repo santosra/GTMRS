a7f03e545aeaca9301156e243f6856fef4dc3bfe
glozano7 <glozano3@gatech.edu>
2014-04-21 10:04:04 -0400

    Update InsertStatements.sql

--- MySQL/InsertStatements.sql
+++ MySQL/InsertStatements.sql
@@ -4,3 +4,5 @@ INSERT INTO Payment_Information
 INSERT INTO Patient
 	VALUES ('LozanoG', 'Gloria', '1990-10-20', 'female', '555 Smallville Drive', 
 	5555555555, 5555555555, 'mom', 1111111111, 120.0, 62.0, 5000.0, '5555555555555555');
+INSERT INTO Admin
+	VALUES ('PetersonB');
