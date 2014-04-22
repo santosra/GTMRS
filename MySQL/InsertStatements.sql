5332f1074613331a1c33392e9d05c104040b92ed
glozano7 <glozano3@gatech.edu>
2014-04-22 17:29:12 -0400

    Update InsertStatements.sql

--- MySQL/InsertStatements.sql
+++ MySQL/InsertStatements.sql
@@ -4,12 +4,12 @@ INSERT INTO Payment_Information
 INSERT INTO Patient
 	VALUES ('LozanoG', 'Gloria', '1990-10-20', 'female', '555 Smallville Drive', 
 	5555555555, 5555555555, 'mom', 1111111111, 120.0, 62.0, 5000.0, '5555555555555555');
+INSERT INTO Patient_Allergies 
+	VALUES ('LozanoG', 'Benadryl');
 INSERT INTO Admin
 	VALUES ('PetersonB');
 INSERT INTO Doctor
 	VALUES ('WilkinsC', 902000, 'Carl', 'Wilkins', '1989-03-10', 
 	4071111111, '123 Alafaya Woods', 'Orthopedics', 101);
-INSERT INTO Patient_Allergies 
-	VALUES ('LozanoG', 'Benadryl');
 INSERT INTO Doctor_Availability
 	VALUES ('WilkinsC', '08:00:00', '10:00:00', '2013-09-11');
