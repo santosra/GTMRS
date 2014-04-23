049ffba2f223a054baf8486a48b3f05a819775cf
Evan Norsworthy <enorswo@gmail.com>
2014-04-23 03:23:53 -0400

    new

--- MySQL/DropTables.sql
+++
@@ -1,6 +0,0 @@
-set foreign_key_checks = 0;
-DROP TABLE IF EXISTS SendsMessageToDoctor, SendsMessageToPatient, Performs, Appointments,
-	CommunicatesWith, Prescription, VisitDiagnosis, Visit, Surgery_PreOp_Meds, Surgery,
-	Doctor_Rating, Doctor_Availability, Patient, Payment_Information, Admin, User, 
-	Doctor, Patient_Allergies;
-set foreign_key_checks = 1;
