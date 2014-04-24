5a3abb4ade1410ec3eeffa72fea817cfd2d5f115
Rebecca Santos <rsantos7@gatech.edu>
2014-04-23 22:38:55 -0400

    Update database information

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 
 // Database configuration
-$db_host = "127.11.142.2"; // Host name
+$db_host = "127.3.58.2"; // Host name
 $db_port = "3306"; // Port number
-$db_username = "adminIbvwfLd"; // Mysql username
-$db_password = "uruasduzqFn_"; // Mysql password
+$db_username = "adminXa3ShrZ"; // Mysql username
+$db_password = "vB7fW-PRX3Ss"; // Mysql password
 $db_name = "gtmrs"; // Database name
 
 // Connect to server and select databse.
