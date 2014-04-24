1cdbfb10d18896a99ec556082f88e7009dda3c4c
glozano7 <glozano3@gatech.edu>
2014-04-24 09:58:44 -0400

    Update Queries.sql

--- MySQL/Queries.sql
+++ MySQL/Queries.sql
@@ -47,85 +47,63 @@ INSERT INTO Patient_Allergies
 
 #New Doctor
 INSERT INTO Doctor
-(licenseNO, firstName, lastName, dob, workPhone, specialty, roomNO, homeAddress, gender, doctor_Availability)
-VALUES ($licenseNO, $firstName, $lastName, $dob, $workPhone, $specialty, $roomNO, $homeAddress, $gender, $doctor_Availability);
+	VALUES ($doctorUsername, $licenseNO, $firstName, $lastName, $dob, $workPhone, $homeAddress, $specialty, $roomNO);
+
+INSERT INTO Doctor_Availability
+	VALUES ($doctorUsername, $startTime, $endTime, $date);
+
 
 #New Admin
 INSERT INTO Administrator
-(username, password)
-VALUES 
-($username, $password);
+	VALUES ($username, $password);
 
 
 /*Update Patient
 repopulate with previous information*/
-SELECT name, homePhone, income, dob, gender, address, workPhone, contactName, contactPhone, weight, height FROM Patient;
-#assume $username is managed by application also username is the primary key
+*attribute=> any value that corresponds to that user
+
+SELECT * FROM Patient;
 UPDATE Patient
-SET name=$name, homePhone=$homePhone, income=$income, dob=$dob, gender=$gender, address=$address , workPhone=$workPhone, contactName=$contactName, contactPhone, weight=$weight, height=$height;
-WHERE username=$username;
+SET attribute=$attribute;
+WHERE patientUsername=$patientUsername;
 
 /*Update Doctor
 repopulate with previous information*/
-SELECT licenseNO, firstName, lastName, dob, workPhone, specialty, roomNO, homeAddress FROM Doctor;
-#assume $username is managed by application also username is the primary key
+
+SELECT* FROM Doctor;
 UPDATE Doctor
-SET licenseNO =$ licenseNO, firstName=$firstName, lastName=$lastName, dob=$dob, workPhone=$workPhone, specialty=$specialty , roomNO=$roomNO, homeAddress=$homeAddress
-WHERE username=$username;
+SET attribute=$attribute 
+WHERE doctorUsername=$doctorUsername;
 
 
 
 #Updating availability
-SELECT date, sTime, eTime
+SELECT *
 FROM Availability
 UPDATE Availibility
-SET date=$date, sTime=$sTime, eTime=$eTime
-WHERE username=$username
-
-/*Appointments
-View doctor info and availability to schedule an appointment 
-They choose the specialty from a dropdown menu
-They choose which time works best for them and click request appointment*/
-SELECT firstName, lastName, Doctor.workPhone, roomNO, to, from, day, rating)
-FROM Doctor, Doctor_Availability, Rates
-WHERE specialty=$specialty
-
-/*Update availability whenever user makes a new appointment
-Update doctor’s appointments whenever user makes a new appointment*/
-SELECT date, sTime, eTime
-FROM Availability
-DELETE date,sTime, eTime
-WHERE 
+SET attribute=$attribute
+WHERE doctorUsername=$doctorUsername
 
 
 
 
 
-
-
-
-
-
-
-
-
-/*Show Appointments to doctor if day selected show all appointments for that day or no day selected show month in general
-when a day is selected
-Availibility
-Assume username is primary key and is managed by application
-Creating availibility*/
-(INSERT INTO Doctor_Availability
-(doctorUsername, startTime, endTime, day)
-VALUES ($doctorUsername, $startTime, $endTime, $day)
+/*View Appointments 
 
 SELECT *
-FROM Availability 
-WHERE Availability.username = Doctor.username AND Availability.date=$date;
+	FROM Availability 
+	WHERE Availability.username = Doctor.username AND Availability.date=$date;
 
-#Patient visit history
-SELECT *
-FROM Visits, Prescription
-WHERE Visits.patient=Patient.name AND Prescription.patient=Patient.name;
+SELECT COUNT*
+	FROM Availability 
+	WHERE MONTH($date) = $month AND YEAR($date)= $year
+	GROUP BY date
+
+/*View visit history
+SELECT Doctor.firstName, dateOfVisit, diastolicPressure, systolicPressure, medicineName, dosage, duration, notes,diagnosis
+	FROM Visits, Prescription, VisitDiagnosis, Doctor
+	WHERE Visits.doctorUsername=Doctor.doctorUsername AND Visit.visitID= VisitDiagnosis.visitID AND Visit.visitID= Prescription.visitID
+	AND Visit.patientUsername= $patientUsername;
 
 
 #Surgery Record
