5de407c72657da545df06c780226ebb84020abf2
Evan Norsworthy <enorswo@gmail.com>
2014-04-25 01:35:09 -0400

    update rateadoctor

--- RateADoctor.php
+++ RateADoctor.php
@@ -16,24 +16,27 @@ $patientUsername = $_POST['username'];
 $sql = "SELECT * FROM Doctor_Rating WHERE doctorUsername='$doctorUsername' 
 	and patientUsername='$patientUsername'";
 $result = $db->query($sql);
+string mysql_error ([ resource $link_identifier = NULL ] )
 $count = $result->num_rows;
 if($count > 1) { //delete all tuples
 	$sql = "DELETE * FROM Doctor_Rating WHERE doctorUsername='$doctorUsername' 
 		and patientUsername='$patientUsername'";
 	$db->query($sql);
+	string mysql_error ([ resource $link_identifier = NULL ] )
 }
 if($count == 0) { //add tuple
 	$rating = (int) $_POST['rating2'];
 	$sql = "INSERT INTO Doctor_Rating(doctorUsername, patientUsername, rating)
 		VALUES ($doctorUsername, $patientUsername, $rating)";
 	$result = $db->query($sql);
+	string mysql_error ([ resource $link_identifier = NULL ] )
 }
 else { //modify existing tuple
 	$sql = "UPDATE Doctor_Rating SET rating = $rating
 		WHERE doctorUsername=$doctorUsername and patientUsername=$patientUsername;";
 	$result = $db->query($sql);
+	string mysql_error ([ resource $link_identifier = NULL ] )
 }
-print('10');
 // Put code here!
 
 
