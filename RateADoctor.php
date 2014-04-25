65dab87fd7d6213d17185d81c5e94c995e3b63c3
Evan Norsworthy <enorswo@gmail.com>
2014-04-25 02:16:40 -0400

    update rateadoctor

--- RateADoctor.php
+++ RateADoctor.php
@@ -9,36 +9,33 @@ if(!isset($_SESSION['username'])){
 require 'database.php';
 require basename(__FILE__, '.php').".html";
 
-$doctorUsername = $_POST['doctorName'];
+/*$doctorUsername = $_POST['doctorName'];
 $patientUsername = $_POST['username'];
 
 //Determine if this patient has rated this Doctor
 $sql = "SELECT * FROM Doctor_Rating WHERE doctorUsername='$doctorUsername' 
 	and patientUsername='$patientUsername'";
 $result = $db->query($sql);
-string mysql_error ([ resource $link_identifier = NULL ] )
 $count = $result->num_rows;
 if($count > 1) { //delete all tuples
 	$sql = "DELETE * FROM Doctor_Rating WHERE doctorUsername='$doctorUsername' 
 		and patientUsername='$patientUsername'";
 	$db->query($sql);
-	string mysql_error ([ resource $link_identifier = NULL ] )
 }
 if($count == 0) { //add tuple
 	$rating = (int) $_POST['rating2'];
 	$sql = "INSERT INTO Doctor_Rating(doctorUsername, patientUsername, rating)
 		VALUES ($doctorUsername, $patientUsername, $rating)";
 	$result = $db->query($sql);
-	string mysql_error ([ resource $link_identifier = NULL ] )
 }
 else { //modify existing tuple
 	$sql = "UPDATE Doctor_Rating SET rating = $rating
 		WHERE doctorUsername=$doctorUsername and patientUsername=$patientUsername;";
 	$result = $db->query($sql);
-	string mysql_error ([ resource $link_identifier = NULL ] )
 }
-// Put code here!
-
+header("location:index.php");
+	ob_end_flush();
+	exit();*/
 
 $db->close();
 
